1631f5a7ac6c8eb0dd3d982004e864eafd660fa0
ramyar-test <mameamrm@gmail.com>
2026-02-19 13:19:25 +0300

    inp

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
 
+    <meta name="google-site-verification" content="PL8s3YFl7fsF0kQF30kI1ZFnDMG2UqRWrbP_q5DLS0c" />
+
     {{-- icon --}}
     <link rel="icon" href="{{ asset('img/logo/full_logo.png') }}" type="image/x-icon" />
 
